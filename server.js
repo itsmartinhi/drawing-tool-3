2467593e2d96f6b05dfdd3412d8b921d6f6109e5
Martin Hintersberger <martin.hintersberger@gmail.com>
2022-08-02 21:01:44 +0200

    update

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const http = require('http');
+
+const app = express()
+app.use(express.json());
+app.use(express.static("express"));
+
+app.get('/*/canvas/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(`Ich bin eine Canvasseite mit der ID: ${id}!`)
+})
+
+app.get('*', (req, res, next) => {
+    res.send(`Ich bin eine Übersichtsseite`)
+})
+
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
