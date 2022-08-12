baddc63b7e45af8d9912b1c875d3999f1ed9427c
Martin Hintersberger <martin.hintersberger@gmail.com>
2022-08-12 15:30:15 +0200

    update file structure

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 // })
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
     // res.send(`Ich bin eine Übersichtsseite`)
 })
 
